e25c96d120e4a258ef901f3e450c4612a6c05c08
tofudd <bartlomiej.tota@outlook.com>
2020-12-02 19:17:47 +0100

    Status messages

--- Lanchat.Core/Relay.cs
+++ Lanchat.Core/Relay.cs
@@ -36,43 +36,61 @@ namespace Lanchat.Core
                     var node = new Node(session, true, true);
                     IncomingConnections.Add(node);
                     node.NetworkInput.MessageReceived += NodeOnMessageReceived;
+                    node.Connected += NodeOnConnected;
                     node.HardDisconnect += OnHardDisconnected;
+                    node.NicknameChanged += NodeOnNicknameChanged;
                     Trace.WriteLine($"Session for {session.Endpoint.Address} created. Session ID: {session.Id}");
                 }
             };
 
             return session;
         }
-
+        
         protected override void OnStarted()
         {
             Trace.WriteLine($"Relay listening on {Endpoint.Port}");
             base.OnStarted();
         }
 
-        private void OnHardDisconnected(object sender, EventArgs e)
-        {
-            var node = (Node) sender;
-            IncomingConnections.Remove(node);
-            node.Dispose();
-        }
-
         private void NodeOnMessageReceived(object sender, string e)
         {
             var nodeNetworkInput = (NetworkInput) sender;
             var node = IncomingConnections.First(x => x.Id == nodeNetworkInput.Id);
-            Trace.WriteLine($"Broadcasting message from {nodeNetworkInput.Id}");
-
-            IncomingConnections.Where(x => x.Id != nodeNetworkInput.Id).ToList().ForEach(x =>
-            {
-                x.NetworkOutput.SendMessage($"{node.Nickname}: {e}");
-            });
+            Broadcast(node.Id,$"{node.Nickname}: {e}");
+        }
+        
+        private void NodeOnConnected(object sender, EventArgs e)
+        {
+            var node = (Node) sender;
+            Broadcast(node.Id,$"{node.Nickname} connected");
+        }
+        
+        private void OnHardDisconnected(object sender, EventArgs e)
+        {
+            var node = (Node) sender;
+            Broadcast(node.Id,$"{node.Nickname} disconnected");
+            IncomingConnections.Remove(node);
+            node.Dispose();
+        }
+        
+        private void NodeOnNicknameChanged(object sender, string e)
+        {
+            var node = (Node) sender;
+            Broadcast(node.Id,$"{e} changed nickname to {node.Nickname}");
         }
 
         protected override void OnError(SocketError error)
         {
             ServerErrored?.Invoke(this, error);
             Trace.WriteLine($"Server errored: {error}");
+        }
+
+        private void Broadcast(Guid senderId, string message)
+        {
+            IncomingConnections.Where(x => x.Id != senderId).ToList().ForEach(x =>
+            {
+                x.NetworkOutput.SendMessage(message);
+            });
         }
     }
 }
